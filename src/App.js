41429d1e7b48ce58dd5ceaa990d4e10b3f8957f9
Arpit <arpit@gokwik.co>
2021-08-16 12:25:54 +0530

    add api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { RedocStandalone } from "redoc";
+// import file from "../Merchant_Dashboard.postman_collection.json";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RedocStandalone
+        specUrl="https://dev-dash.gokwik.co/public/Gokwik_Custom_API_Integration_Docs.json"
+        options={{
+          nativeScrollbars: true,
+          theme: { colors: { primary: { main: "#dd5522" } } },
+        }}
+      />
     </div>
   );
 }
